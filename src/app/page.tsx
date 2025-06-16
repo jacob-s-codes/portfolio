aa484e1800ffc0fe1b4cbfbe2b1e07018d79d37b
jacob-s-codes <jacobbshaul@gmail.com>
2025-06-15 22:48:28 -0700

    updated projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,19 +47,19 @@ export default function Home() {
           />
 
           <div className="w-full bg-lessdarkbg flex flex-col gap-y-6 justify-center">
-            <h4 className="border mx-4 border-white w-fit px-6 py-1 rounded-3xl text-sm uppercase">Founder, frontend development</h4>
+            <h4 className="border mx-4 border-white w-fit px-6 py-1 rounded-3xl text-sm uppercase mt-6">Founder, frontend development</h4>
             <div className="px-4">
               <h3 className="text-5xl font-semibold uppercase">Mode to Code</h3>
               <hr className="mr-8" />
             </div>
             <p className="text-xl px-4">Founder of Mode to Code, a program that teaches students of all ages about coding and technology.</p>
             <a href="/modetocode"><button className="w-fit mx-4 hover:cursor-pointer border-white border px-6 py-2 hover:bg-darkbg duration-200 ease-in hover:rounded-r-xl">Read more</button></a>
-            <div className="flex flex-row items-center justify-between gap-x-8 pt-4 px-4 border-t-16 border-darkbg">
-              <div className="flex flex-col items-start">
+            <div className="flex flex-row items-stretch justify-between gap-x-8 px-4 border-t-16 border-darkbg h-full">
+              <div className="flex flex-col items-start border-darkbg border-r-16 pt-6 h-full">
                 <h5 className="font-extrabold text-4xl uppercase">850+</h5>
                 <p>Taught over 850 students around the world about coding.</p>
               </div>
-              <div className="flex flex-col items-start">
+              <div className="flex flex-col items-start pt-6 h-full"> 
                 <h5 className="font-black text-4xl uppercase">15</h5>
                 <p>Worked with 15 student volunteers.</p>
               </div>
@@ -76,19 +76,19 @@ export default function Home() {
           />
 
           <div className="w-full bg-lessdarkbg flex flex-col gap-y-6 justify-center">
-            <h4 className="border mx-4 border-white w-fit px-6 py-1 rounded-3xl text-sm uppercase">Organizer, frontend development, outreach</h4>
+            <h4 className="border mx-4 border-white w-fit px-6 py-1 rounded-3xl text-sm uppercase mt-12">Organizer, frontend development, outreach</h4>
             <div className="px-4">
               <h3 className="text-5xl font-semibold uppercase">UHS Hacks</h3>
               <hr className="mr-8" />
             </div>
             <p className="text-xl px-4">Organized my high school's inaugural hackathon, bringing high school students from around the bay area to come build.</p>
             <a href="/uhshacks"><button className="w-fit mx-4 hover:cursor-pointer border-white border px-6 py-2 hover:bg-darkbg duration-200 ease-in hover:rounded-r-xl">Read more</button></a>
-            <div className="flex flex-row items-center justify-between gap-x-8 pt-4 px-4 border-t-16 border-darkbg">
-              <div className="flex flex-col items-start">
+            <div className="flex flex-row items-stretch justify-between gap-x-8 px-4 border-t-16 border-darkbg h-full">
+              <div className="flex flex-col items-start border-r-16 border-darkbg h-full pt-4 ">
                 <h5 className="font-extrabold text-4xl uppercase">50</h5>
                 <p>50 students participated for free at UHS Hacks.</p>
               </div>
-              <div className="flex flex-col items-start">
+              <div className="flex flex-col items-start pt-4 h-full">
                 <h5 className="font-black text-4xl uppercase">8</h5>
                 <p>Organized with 8 code support and outreach volunteers.</p>
               </div>
