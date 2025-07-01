0e85d53316fe56c7ad4c06c017ffe4769f807914
jacob-s-codes <jacobbshaul@gmail.com>
2025-06-30 23:15:39 -0700

    updated mtc page

--- src/app/modetocode/page.tsx
+++ src/app/modetocode/page.tsx
@@ -2,10 +2,15 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
+    <div className=''>
         <h1 className='text-7xl  pt-6'>Mode to Code</h1>
         <hr className='border-2 border-white'/>
-        {/* <img src="/" alt="" /> */}
+        <div className='w-full flex items-center justify-center mt-8'>
+            <img src="/mtc/teachingimg17.jpg" alt="Mode to Code logo" className='rounded-lg' />
+        </div>
+
+
+
     </div>
   )
 }
