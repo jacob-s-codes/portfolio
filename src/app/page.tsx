e914b1c4a49c80752b96c03b0fe2aa73e5a5068b
jacob-s-codes <jacobbshaul@gmail.com>
2025-08-13 10:40:44 -1000

    updated meta data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 // import Image from "next/image";
 
 import HomeClient from "./HomeClient";
-
-export const metadata = { 
+import type { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
   title: "Jacob Shaul | Home",
   description: "Hi! My name is Jacob Shaul, a full-stack high school developer.",
 }
+
 
 export default function Home() {
   return (
